de051774d73f52c9b5308d4e198a93f6283ffe08
Feisal Hassan <faisalabdi493@gmail.com>
2025-09-09 18:32:51 +0300

    first commit

--- server/app.py
+++ server/app.py
@@ -22,6 +22,38 @@ def index():
 
 # Add views here
 
+from flask import jsonify
+
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    quake = Earthquake.query.filter_by(id=id).first()
+    if quake:
+        return jsonify({
+            "id": quake.id,
+            "location": quake.location,
+            "magnitude": quake.magnitude,
+            "year": quake.year
+        }), 200
+    else:
+        return jsonify({"message": f"Earthquake {id} not found."}), 404
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    quake_list = [
+        {
+            "id": q.id,
+            "location": q.location,
+            "magnitude": q.magnitude,
+            "year": q.year
+        } for q in quakes
+    ]
+    return jsonify({
+        "count": len(quake_list),
+        "quakes": quake_list
+    }), 200
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
